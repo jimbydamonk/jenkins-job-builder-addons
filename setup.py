ae108984af88d484a5ca9baff68dfbf3587ffbe5
Mike Buzzetti <mike.buzzetti@gmail.com>
2015-08-13 08:08:29 -0400

    Add setup.py test to use tox

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from setuptools.command.test import test as TestCommand
 
 try:
     from setuptools import setup
@@ -21,6 +21,28 @@ requirements = [
 test_requirements = [
     # TODO: put package test requirements here
 ]
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        tox.cmdline(args=args)
 
 setup(
     name='jenkins-job-builder-addons',
@@ -49,7 +71,8 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=['tox'] + test_requirements,
+    cmdclass={'test': Tox},
     entry_points={
         'jenkins_jobs.projects': [
             'folder=jenkins_jobs_addons.folders:Folder',
